4723ce408923b4d13758a09751e8eaa30269de08
Léon <git.staleness589@passmail.com>
2026-04-22 09:29:14 +0200

    formatting trials

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,84 @@
+use std::fmt; // Import `fmt`
+
+// A structure holding two numbers. `Debug` will be derived so the results can
+// be contrasted with `Display`.
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+// Implement `Display` for `MinMax`.
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.imag >= 0.0 {
+            write!(f, "{} + {}i", self.real, self.imag)
+        } else {
+            write!(f, "{} - {}i", self.real, self.imag.abs()) // ! abs est mieux pour gérer les nombres négatifs, on controle vraiment l'affichage
+        }
+    }
+}
+
+// Define a structure where the fields are nameable for comparison.
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// Similarly, implement `Display` for `Point2D`.
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
 fn main() {
-    // In general, the `{}` will be automatically replaced with any
-    // arguments. These will be stringified.
-    println!("{} days", 31);
-
-    // Positional arguments can be used. Specifying an integer inside `{}`
-    // determines which additional argument will be replaced. Arguments start
-    // at 0 immediately after the format string.
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
-
-    // As can named arguments.
-    println!("{subject} {verb} {object}",
-             object="the lazy dog",
-             subject="the quick brown fox",
-             verb="jumps over");
-
-    // Different formatting can be invoked by specifying the format character
-    // after a `:`.
-    println!("Base 10:               {}",   69420); // 69420
-    println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
-    println!("Base 8 (octal):        {:o}", 69420); // 207454
-    println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
-
-    // You can right-justify text with a specified width. This will
-    // output "    1". (Four white spaces and a "1", for a total width of 5.)
-    println!("{number:a>5}", number=1);
-
-    // You can pad numbers with extra zeroes,
-    println!("{number:0>5}", number=1); // 00001
-    // and left-adjust by flipping the sign. This will output "10000".
-    println!("{number:0<5}", number=1); // 10000
-
-    // You can use named arguments in the format specifier by appending a `$`.
-    println!("{number:1>width$}", number=1, width=5); //     1
-
-    // Rust even checks to make sure the correct number of arguments are used.
-    println!("My name is {0}, {1} {0}", "Bond", "ANTOINE DUPONT");
-    // FIXME ^ Add the missing argument: "James"
-
-    // Only types that implement fmt::Display can be formatted with `{}`. User-
-    // defined types do not implement fmt::Display by default.
-
-    #[allow(dead_code)] // disable `dead_code` which warn against unused module
-    struct Structure(i32);
-
-    // This will not compile because `Structure` does not implement
-    // fmt::Display.
-    //println!("This struct `{}` won't print...", Structure(3));
-    // TODO ^ Try uncommenting this line
-
-    // For Rust 1.58 and above, you can directly capture the argument from a
-    // surrounding variable. Just like the above, this will output
-    // "    1", 4 white spaces and a "1".
-    let number: f64 = 1.0;
-    let width: usize = 5;
-    println!("{number:>width$}");
+    let complex = Complex{real:3.3, imag:7.2};
+    println!("Compare complex numbers:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+    
+    let complexneg = Complex{real:3.3, imag:-7.2};
+    println!("Compare complex numbers:");
+    println!("Display: {}", complexneg);
+    println!("Debug: {:?}", complexneg);
     
     
-    let _pi = 3.14159265358979;
-    let precision: usize = 6;
-    println!("Pi is roughly {_pi:.0$}", precision);
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range =   MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range);
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // Error. Both `Debug` and `Display` were implemented, but `{:b}`
+    // requires `fmt::Binary` to be implemented. This will not work.
+    //println!("What does Point2D look like in binary: {:b}?", point);
+    
+    
+    
 }

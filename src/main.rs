137f12a6be07678872801cd7d588b12bd4e80bb7
Léon <git.staleness589@passmail.com>
2026-06-03 23:35:58 +0200

    t

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,84 @@
-use std::fmt; // Import the `fmt` module.
+use std::fmt; // Import `fmt`
 
-// Define a structure named `List` containing a `Vec`.
-struct List(Vec<i32>);
+// A structure holding two numbers. `Debug` will be derived so the results can
+// be contrasted with `Display`.
+#[derive(Debug)]
+struct MinMax(i64, i64);
 
-impl fmt::Display for List {
+// Implement `Display` for `MinMax`.
+impl fmt::Display for MinMax {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Extract the value using tuple indexing,
-        // and create a reference to `vec`.
-        let vec = &self.0;
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
 
-        write!(f, "[")?;
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
 
-        // Iterate over `v` in `vec` while enumerating the iteration
-        // index in `index`.
-        for (index, v) in vec.iter().enumerate() {
-            // For every element except the first, add a comma.
-            // Use the ? operator to return on errors.
-            if index != 0 { write!(f, ", ")?; }
-            write!(f, "{}", v)?;
+impl fmt::Display for Complex{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.imag >= 0.0 {
+            write!(f, "{} + {}i", self.real, self.imag)
+        } else {
+            write!(f, "{} - {}i", self.real, self.imag.abs()) // ! abs est mieux pour gérer les nombres négatifs, on controle vraiment l'affichage
         }
+    }
+}
 
-        // Close the opened bracket and return a fmt::Result value.
-        write!(f, "]")
+// Define a structure where the fields are nameable for comparison.
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// Similarly, implement `Display` for `Point2D`.
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
     }
 }
 
 fn main() {
-    let v = List(vec![1, 2, 3]);
-    println!("{}", v);
+    let complex = Complex{real:3.3, imag:7.2};
+    println!("Compare complex numbers:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+    
+    let complexneg = Complex{real:3.3, imag:-7.2};
+    println!("Compare complex numbers:");
+    println!("Display: {}", complexneg);
+    println!("Debug: {:?}", complexneg);
+    
+    
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range =   MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range);
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // Error. Both `Debug` and `Display` were implemented, but `{:b}`
+    // requires `fmt::Binary` to be implemented. This will not work.
+    //println!("What does Point2D look like in binary: {:b}?", point);
+    
+    
+    
 }
